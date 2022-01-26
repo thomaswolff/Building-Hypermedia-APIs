578d54bd0e864dc6ae2c4c29da165aed02b3c4ec
Thomas Wolff <thomas.wolff@nrk.no>
2022-01-26 11:07:25 +0100

    Fikset add-task.js

--- nodejs/collection/cmd-line-app/add-task.js
+++ nodejs/collection/cmd-line-app/add-task.js
@@ -7,65 +7,67 @@ var http = require('http');
 var client, host, port, path, args, help;
 host = 'localhost';
 port = 3000;
-path = '/collection/tasks/';
+path = 'http://localhost:3000/collection/tasks/';
 hdrs = {};
 args = {};
 
 help = '*** Usage:\n' +
-   'node add-task.js "<description>" "<dueDate>" [<completed>]\n' +
-   '  Where:\n' +
-   '    <description> is text of task (in quotes)\n' +
-   '    <dueDate> is YYYY-MM-DD (in quotes)\n' +
-   '    <completed> is true|false';
-   
+  'node add-task.js "<description>" "<dueDate>" [<completed>]\n' +
+  '  Where:\n' +
+  '    <description> is text of task (in quotes)\n' +
+  '    <dueDate> is YYYY-MM-DD (in quotes)\n' +
+  '    <completed> is true|false';
+
 // check args and fire off processing
-if(process.argv.length<4 || process.argv.length>5) {
+if (process.argv.length < 4 || process.argv.length > 5) {
   console.log(help);
 }
 else {
   args.description = process.argv[2];
   args.dateDue = process.argv[3];
-  args.completed = (process.argv[4]||false);
+  args.completed = (process.argv[4] || false);
 
-  client = http.createClient(port,host);
+  client = http;
   getTemplate();
 }
 
 // get the server's write template
 function getTemplate() {
   // request the template
-  var req = client.request('GET', path, {'host':host+':'+port});
+  var options = { method: "GET" }
+  var req = client.request(path, options, (res) => {
 
-  // event handlers
-  req.on('response', function(response) {
+    if (res.statusCode !== 200) {
+      console.error("Did not get an OK from the server. Code: " + res.statusCode)
+    }
+
     var body = '';
-    response.on('data', function(chunk) {
+    res.on('data', function (chunk) {
       body += chunk;
     });
-    response.on('error', function(error) {
+    res.on('error', function (error) {
       console.log(error);
     });
-    response.on('end', function() {
+    res.on('end', function () {
       var data = JSON.parse(body);
       buildTask(data.collection.template);
     });
-  });
 
-  req.on('error', function(error) {
-    console.log(error);
+    res.on('error', function (error) {
+      console.log(error);
+    });
   });
-
   req.end();
 }
 
 // write the task data to the server
 function buildTask(template) {
   var coll, i, x, msg;
-  
+
   // populate the template
   coll = template.data;
-  for(i=0,x=coll.length;i<x;i++) {
-    switch(coll[i].name) {
+  for (i = 0, x = coll.length; i < x; i++) {
+    switch (coll[i].name) {
       case 'description':
         coll[i].value = args.description;
         break;
@@ -75,44 +77,42 @@ function buildTask(template) {
       case 'completed':
         coll[i].value = args.completed;
         break;
-    }  
+    }
   }
-  msg = '{"template" : '+JSON.stringify(template)+"}";
+  msg = '{"template" : ' + JSON.stringify(template) + "}";
   sendData(msg);
 }
 
 // send the data to the server for storage
 function sendData(msg) {
-  // set up request
-  var hdrs = {
-    'host' : host+':'+port,
-    'content-type' : 'application/collection+json',
-    'content-length' : (msg.length)
-  };
-  
+  var options = {
+    method: "POST",
+    headers: {
+      'host': host + ':' + port,
+      'content-type': 'application/collection+json',
+      'content-length': (msg.length)
+    }
+  }
   // pass data to the server
-  var req = client.request('POST', path, hdrs);
-  req.write(msg);
-
-  // event handlers
-  req.on('response', function(response) {
+  var req = client.request(path, options, (res) => {
     var body = '';
-    response.on('data', function(chunk) {
+    res.on('data', function (chunk) {
       body += chunk;
     });
-    response.on('error', function(error) {
+    res.on('error', function (error) {
       console.log(error);
     });
-    response.on('end', function() {
+    res.on('end', function () {
       console.log('*** task added!');
     });
   });
+  req.write(msg);
 
-  req.on('error', function(error) {
+  req.on('error', function (error) {
     console.log(error);
   });
 
-  req.end();  
+  req.end();
 }
 
 // eof
